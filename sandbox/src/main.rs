bf21716d364dd436ca3849df08c8ac0fbba892ea
444B <15311579+444B@users.noreply.github.com>
2023-12-21 12:22:52 +0200

    works with ipv6 and included a gitignore for target/

--- sandbox/src/main.rs
+++ sandbox/src/main.rs
@@ -5,7 +5,8 @@ use std::net::SocketAddr;
 
 #[tokio::main]
 async fn main() {
-    let addr = SocketAddr::from(([127, 0, 0, 1], 8080));
+    // Specify the IPv6 address and port here
+    let addr = SocketAddr::from(([0, 0, 0, 0, 0, 0, 0, 1], 8080));
 
     let make_svc = make_service_fn(|_conn| async {
         Ok::<_, Infallible>(service_fn(|_req| async {
@@ -15,10 +16,9 @@ async fn main() {
 
     let server = Server::bind(&addr).serve(make_svc);
 
-    println!("Starting server at port 8080");
+    println!("Starting server at port 8080 on IPv6");
 
     if let Err(e) = server.await {
         eprintln!("server error: {}", e);
     }
 }
-
