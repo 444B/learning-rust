d476f39f8d708f8464b62769e7d44e15126213ea
444B <15311579+444B@users.noreply.github.com>
2023-12-21 08:47:56 +0200

    basic http server

---
+++ sandbox/src/main.rs
@@ -0,0 +1,24 @@
+use hyper::{Body, Response, Server};
+use hyper::service::{make_service_fn, service_fn};
+use std::convert::Infallible;
+use std::net::SocketAddr;
+
+#[tokio::main]
+async fn main() {
+    let addr = SocketAddr::from(([127, 0, 0, 1], 8080));
+
+    let make_svc = make_service_fn(|_conn| async {
+        Ok::<_, Infallible>(service_fn(|_req| async {
+            Ok::<_, Infallible>(Response::new(Body::from("Hello World")))
+        }))
+    });
+
+    let server = Server::bind(&addr).serve(make_svc);
+
+    println!("Starting server at port 8080");
+
+    if let Err(e) = server.await {
+        eprintln!("server error: {}", e);
+    }
+}
+
